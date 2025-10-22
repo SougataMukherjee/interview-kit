271f29a839df757d2b6e533925537df61145432d
SougataMukherjee <49196315+SougataMukherjee@users.noreply.github.com>
2025-10-22 21:18:36 +0530

    add theory notes

---
+++ technical/Counter.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h2>Counter: {count}</h2>
+      <button onClick={increment}> + </button>
+      <button onClick={decrement}> - </button>
+    </div>
+  );
+}
