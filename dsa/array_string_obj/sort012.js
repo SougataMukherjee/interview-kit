708737ec7615bb30defeb1f350ccc2065f0a67df
SougataMukherjee <49196315+SougataMukherjee@users.noreply.github.com>
2025-10-31 13:50:43 +0530

    restructure theory notes

--- dsa/array_string_obj/sort012.js
+++ dsa/array_string_obj/sort012.js
@@ -1,13 +1,17 @@
 function sort012(arr) {
   let count0 = 0, count1 = 0, count2 = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === 0) count0++;
-    else if (arr[i] === 1) count1++;
+
+  for (let num of arr) {
+    if (num === 0) count0++;
+    else if (num === 1) count1++;
     else count2++;
   }
-  let res = [];
-  for (let i = 0; i < count0; i++) res.push(0);
-  for (let i = 0; i < count1; i++) res.push(1);
-  for (let i = 0; i < count2; i++) res.push(2);
-  return res;
+
+  return [
+    ...Array(count0).fill(0),
+    ...Array(count1).fill(1),
+    ...Array(count2).fill(2)
+  ];
 }
+
+console.log(sort012([2, 0, 1, 2, 1, 0]));// [0, 0, 1, 1, 2, 2]
