e3b09c5151985efd2bfd2d29ced82ea2b7e4cbf9
SougataMukherjee <49196315+SougataMukherjee@users.noreply.github.com>
2025-11-15 16:46:41 +0530

    restructure technical notes

--- dsa/array_string_obj/obj/ObjectToGroup.js
+++ dsa/array_string_obj/obj/ObjectToGroup.js
@@ -1,16 +1,10 @@
 const users = [
-
   {name: 'n1', date: '01-02-2025', time: '11-AM', deposit: '100' },
-
   {name: 'n2', date: '01-02-2025', time: '12-AM', deposit: '150' },
-
   {name: 'n1', date: '02-02-2025', time: '10-AM', deposit: '200' },
-
   {name: 'n2', date: '02-02-2025', time: '11-AM', deposit: '180' }
-
 ];
 const output = {};
-
 users.forEach(item => {
   if (!output[item.date] || output[item.date].deposit < item.deposit) {
     output[item.date] = { ...item, max: item.name };
