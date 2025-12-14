2a05f26de07ce9e294e94ea71e3cc8f75c65e991
SougataMukherjee <49196315+SougataMukherjee@users.noreply.github.com>
2025-12-14 21:08:48 +0530

    restructure technical notes

--- technical/ssr-data-fetch-nextjs/page.jsx
+++ technical/ssr-data-fetch-nextjs/page.jsx
@@ -20,3 +20,23 @@ export default function Page() {
     </div>
   );
 }
+
+// export default async function Home() {
+//   const res = await fetch(
+//     "https://jsonplaceholder.typicode.com/todos/1",
+//     {
+//       cache: "no-store", // 👈 SSR for ISR use next: { revalidate: 10 },
+//     }
+//   );
+
+//   const data = await res.json();
+
+//   return (
+//     <div>
+//       <h1>SSR Example (no-store)</h1>
+//       <p>Todo Title: {data.title}</p>
+//       <p>Data fetched on every request</p>
+//     </div>
+//   );
+// }
+
