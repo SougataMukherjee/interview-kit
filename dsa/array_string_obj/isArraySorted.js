8e6afd0d8fa00836b1a2754e1d85166357d71059
SougataMukherjee <49196315+SougataMukherjee@users.noreply.github.com>
2026-01-26 17:30:47 +0530

    restructure technical notes

--- dsa/array_string_obj/isArraySorted.js
+++ dsa/array_string_obj/isArraySorted.js
@@ -1,9 +1,16 @@
 function isSorted(arr) {
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] < arr[i - 1]) return false;
-  }
-  return true;
+    let count = 0;
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < arr[i - 1]){
+          count++;
+        }
+    }
+    //rotation condition
+    if (arr[0] < arr[arr.length - 1]) {
+      count++;
+    }
+  return count <= 1;
 }
-console.log(isSorted([1,2,3,4]))//true
-
-
+console.log(isSorted([6,7,1,2,3,4,5]))//true
+console.log(isSorted([1,2,3,4,5,6,7]))//true
+console.log(isSorted([1,2,3,4,6,5,7]))//false
--- dsa/array_string_obj/toTitleCase.js
+++ dsa/array_string_obj/toTitleCase.js
@@ -1,8 +1,13 @@
 function toTitleCase(str) {
   return str
-    .toLowerCase()
     .split(" ")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(" ");
+    .map((word) =>{
+       // if word is fully uppercase, keep as it is
+      if (word === word.toUpperCase()) {
+        return word;
+      }
+      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+     }).join(" ");
 }
 console.log(toTitleCase('hello world'));// Hello World
+console.log(toTitleCase('WELCOME to the JUNGLE'))//WELCOME To The JUNGLE
